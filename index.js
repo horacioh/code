261e5301227c88aba0d3f0f76ab09d3494e3d49a
Horacio Herrera <hi@horacioh.com>
2020-09-02 17:28:29 +0200

    codigo final

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const lista = ["Nader", "John", "Rich", "Sean", "Dan", "Kyle"]
+
+function tresAleatorios(lista) {
+  return [...lista].sort(() => (Math.random() > 0.5 ? 1 : -1)).slice(0, 3)
+}
+
+console.log(tresAleatorios(lista))
